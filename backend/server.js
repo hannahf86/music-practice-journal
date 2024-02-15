a4e0653123f0dc1c35a8fefd140637a0bd3a09fb
hannahf86 <hannahfeehan.design@gmail.com>
2024-02-15 18:40:51 +0000

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+
+// start the app
+const app = express();
+
+
+// route handler
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+});
+
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT);
+});
